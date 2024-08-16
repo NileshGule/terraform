0d9668eaed62b72666d7f0b1027ac1e9c6197fb8
Nilesh Gule <vn_nilesh@yahoo.com>
2024-08-16 13:46:42 +1000

    terraform variables extension

--- ResourceGroup/variables.tf
+++ ResourceGroup/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" {
-    default = "tfResourceGroup"
-    type = string
-    description = "Name of the resource group"
+  default     = "tfResourceGroup"
+  type        = string
+  description = "Name of the resource group"
 }
 
 variable "resource_group_location" {
-    default = "Australia Southeast" #also works with AustraliaSoutheast
-    type = string
-    description = "Name of the resource group location"
+  default     = "Australia Southeast" #also works with AustraliaSoutheast
+  type        = string
+  description = "Name of the resource group location"
 }
